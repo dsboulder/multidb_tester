79e9c1bd495c390b271a5bf9b3fea630913888a6
David Stevenson <stevend@davidmac.local>
2008-10-06 11:49:11 -0700

    ds: preliminary rspec support

--- lib/multidb_tester.rb
+++ lib/multidb_tester.rb
@@ -1,15 +1,17 @@
 # MultidbTester
 require File.join(File.dirname(__FILE__), 'multidb_tester', 'testcase_extensions')
 require File.join(File.dirname(__FILE__), 'multidb_tester', 'schema_extensions')
+require File.join(File.dirname(__FILE__), 'multidb_tester', 'rspec_options_extensions')
 require File.join(File.dirname(__FILE__), 'multidb_tester', 'utilities')
+
+module MultidbTester
+  mattr_accessor :enabled
+  mattr_accessor :connection_spec_name
+  mattr_accessor :rspec
+  
+  self.enabled = true
+end
 
 Test::Unit::TestCase.send(:include, MultidbTester::TestcaseExtensions)
 ActiveRecord::Schema.send(:extend, MultidbTester::SchemaExtensions)
-
-module MultidbTester
-  include MultidbTester::Utilities
-  
-  mattr_accessor :enabled
-  mattr_accessor :connection_spec_name
-  self.enabled = true
-end
+Spec::Runer::Options.send(:extend, MultidbTester::SchemaExtensions) if MultidbTester.rspec
